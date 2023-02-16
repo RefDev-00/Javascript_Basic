b6bb1bd4bf820da962ddc85458b20cc8b9a3afe1
Refsi Maulana <refsimaulanatkr@gmail.com>
2023-02-17 00:42:28 +0700

    new file javascript

---
+++ js/javascript_dom.js
@@ -0,0 +1,59 @@
+// MANIPULATION DATA
+document.title = 'Refsi Maulana' //document title (mengganti title yang ada di html menggunakan javascript)
+const body = document.body //variabel document body
+
+// Membuat variabel untuk element h1 menggunakan textcontent
+const h1 = document.createElement('h1')
+h1.textContent = 'Ini Belajar Javascript Dasar'
+
+// Membuat variabel untuk element <p> menggunakan innerHTML
+const paragraf = document.createElement('p')
+paragraf.innerHTML = '<marquee>lorem ipsum dolor ya gays ya</marquee>'
+
+// Membuat variabel untuk element h2 menggunakan innertext
+const h2 = document.createElement('h2')
+h2.innerText = 'Ini judul javascript yang ke 2 ya gays ya'
+
+//untuk memanggil variabel agar masuk ke dalam body HTML
+body.append(h1)
+body.append(paragraf)
+body.append(h2)
+
+
+// EVENT & STYLING
+
+// membuat variabel untuk class yang ada di button HTML
+const btnOKe = document.getElementById('btn_oke')
+const btnCancel = document.querySelector('.btn_cancel')
+
+const oriText = 'Oke'
+btnOKe.innerText = oriText 
+ 
+btnOKe.style.border = 'none'
+btnOKe.style.fontSize = '30px'
+btnOKe.style.color = 'white'
+btnOKe.style.background = 'blue'
+
+btnCancel.style.border = 'none'
+btnCancel.style.fontSize = '30px'
+btnCancel.style.color = 'white'
+btnCancel.style.background = 'blue'
+
+
+function tekanTombolOke() {
+  const newText = document.createElement('p')
+  newText.textContent = 'Ini udah disave bro'
+  body.append(newText)
+}
+
+function tekanTombolCancel() {
+  alert('Saya di cancel')
+}
+
+function gantiText() {
+  btnOKe.textContent = 'Cancel'
+}
+
+function textOri() {
+  btnOKe.textContent = oriText
+}
