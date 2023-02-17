c488427cb934da77e1317c9609e959cfe73889d8
Refsi Maulana <refsimaulanatkr@gmail.com>
2023-02-18 00:12:42 +0700

    add new file

---
+++ js/function.js
@@ -0,0 +1,8 @@
+// Membuat function di Javascript
+
+// function menggunakan argument(parameter)
+function addName(firstname, lastname) {
+  console.log(firstname, lastname)
+}
+
+addName('Refsi Maulana', 'Siregar')
