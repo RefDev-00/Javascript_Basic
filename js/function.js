3552d2fc5f4975473411d535a4edd9e4502ff456
Refsi08 <refsimaulanatkr@gmail.com>
2023-02-22 11:43:14 +0700

    new file javascript

--- js/function.js
+++ js/function.js
@@ -18,7 +18,7 @@ addName("Refsi Maulana", "Siregar");
 sum1(2, 3);
 sum2(5, 8);
 
-function calculate(x,y) {
+function calculate(x, y) {
   const total = x + y;
   if (total > 10) {
     console.log("sesuai dengan prediksi dengan angka ", total);
@@ -26,4 +26,56 @@ function calculate(x,y) {
     console.log(total);
   }
 }
-calculate(2,9);
+calculate(2, 9);
+
+// membuat pengejaan dengan function javascript
+let bahasaPemrograman = "Dart";
+
+function basicLanguage() {
+  let bahasaPemrograman = prompt("Input Bahasa Pemrograman");
+  for (let i = 0; i < bahasaPemrograman.length; i++) {
+    const row = bahasaPemrograman[i];
+    if (bahasaPemrograman.toLowerCase() == "dart") {
+      alert(`Huruf ke ${i + 1} ${row}`);
+    } else {
+      alert(bahasaPemrograman);
+    }
+  }
+  alert(`maka hasilnya adalah huruf "${bahasaPemrograman}"`);
+}
+
+basicLanguage(bahasaPemrograman);
+console.log(bahasaPemrograman);
+
+// Function Expression
+// membuat fungsi menjadi lebih ringkas
+const hasilPerkalian = function (nilaiA, nilaiB) {
+  return nilaiA * nilaiB;
+};
+console.log(hasilPerkalian(2, 3));
+
+// Function di dalam functiom
+function duaKali(func) {
+  func();
+  func();
+}
+
+function lemparDadu() {
+  const nilai = Math.floor(Math.random() * 6) + 1;
+  console.log(`Hasil Dari lempar dadu menggunakan 2 function adalah ${nilai}`);
+}
+
+duaKali(lemparDadu);
+
+// Method
+// sebuh function yang ada di dalam objek
+
+const Aritmatika = {
+  perkalian: function (x, y) {
+    return x * y;
+  },
+};
+
+console.log(
+  `Hasil dari method perkalian adalah ${Aritmatika.perkalian(10, 10)}`
+);
